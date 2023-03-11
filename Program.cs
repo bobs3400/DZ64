4f3e682065a9666f3d18db4ab1ec26161bb522b2
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-03-11 16:40:30 +1000

    64

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число N: ");
+int n = int.Parse(ReadLine()!);
+WriteLine();
+WriteLine(PrintNumbers(n));
+
+string PrintNumbers(int n)
+{
+    if(n == 1)
+    {
+        //WriteLine(n);
+        return n.ToString();
+    }
+    string s = n.ToString() + ", " + PrintNumbers(n - 1);
+    //WriteLine(s);
+    return s;
+}
